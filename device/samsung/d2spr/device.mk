d3933025d88ffce03f1340a46c2d69377de23a2a
task650 <task650@gmail.com>
2012-09-17 05:13:15 -0700

    Adds correct APNs

--- device/samsung/d2spr/device.mk
+++ device/samsung/d2spr/device.mk
@@ -23,6 +23,10 @@ DEVICE_PACKAGE_OVERLAYS += device/samsung/d2spr/overlay
 ## common overlays
 DEVICE_PACKAGE_OVERLAYS += device/samsung/d2-common/overlay-cdma
 
+#APN Copy	  	
+PRODUCT_COPY_FILES := device/sample-d2spr/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+
 # Inherit from d2-common
 $(call inherit-product, device/samsung/d2-common/d2-common.mk)
 
